3c31c77dd79d1246cd940df1ea75d455859db2e5
AdrianDF1 <AdrianDF1@users.noreply.github.com>
2019-10-17 17:27:50 +0300

    program

--- Proiect/src/utils/Main.java
+++ Proiect/src/utils/Main.java
@@ -1,5 +1,30 @@
 package utils;
-
+import java.util.Scanner;
 public class Main {
+	public static void main ( String [] args)
+	{
+		Scanner nume =new Scanner(System.in);
+		System.out.println("Alege Materia:");
+		System.out.println("1_Romana");
+		System.out.println("2._Mate");
+		System.out.println("3._Biologie");
+		System.out.println("4._Chimie");
+		Integer optiune=nume.nextInt();
+		String materiaAleasa="";
+		System.out.println("Optiune aleasa=" +optiune);
+		System.out.println("-------------");
+		switch(optiune){
+		case 1:
+			System.out.println("Ai ales Romana");
+			materiaAleasa = "Romana";
+			break;
+			Case 2:
+				System.out.println("Ai ales Mate");
+			materiaAleasa
+			
+		}
+		
+	}
+	}
 
-}
+
